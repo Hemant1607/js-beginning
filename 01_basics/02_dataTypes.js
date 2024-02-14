ab9434790a6c42f5e78e412d768d1e6b399097e2
Hemant1607 <79895722+Hemant1607@users.noreply.github.com>
2024-02-14 18:39:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // whenever we use this - treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+let name = "Hemant"
+let age = 25
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//Object
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
+
+
+
+
+
