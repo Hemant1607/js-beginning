87d0c188fb29008a9277579b03c7b812d9154b7a
Hemant1607 <79895722+Hemant1607@users.noreply.github.com>
2024-03-18 17:51:30 +0000

    Function scope and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,54 @@ if(true)
 //console.log(b);
 console.log(c);
 
+
+function one()
+{
+    const userName = "Hemant";
+
+    function two()
+    {
+        const website = "Youtube";
+        console.log(userName);
+    }
+
+    //console.log(website);
+
+    two();
+}
+
+one();
+
+if(true)
+{
+    const userName = "Hemant"
+
+    if(userName === "Hemant"){
+        const website = "YouTube"
+        console.log(userName + website);
+    }
+
+    //console.log(website);
+}
+
+//console.log(userName);
+
+
+//============================================= Interesting ========================================
+
+console.log(addOne(5));
+function addOne(num)
+{
+    return num + 1
+}
+
+
+//Function expression
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5);
+
+
+
